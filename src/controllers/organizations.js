6a6258f59b30580cf4a3019ddf1dde92150e17fc
josedvega <davarela231@gmail.com>
2026-09-22 23:17:56 -0600

    W04 Learning Activity: Sessions and Flash Messages

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,5 +1,6 @@
-import { getAllOrganizations, getOrganizationDetails } from '../models/organizations.js';
+import { getAllOrganizations, getOrganizationDetails, createOrganization } from '../models/organizations.js';
 import { getProjectsByOrganizationId } from '../models/projects.js';
+import '../middleware/flash.js';
 
 const showOrganizationsPage = async (req, res) => {
     const organizations = await getAllOrganizations();
@@ -7,7 +8,6 @@ const showOrganizationsPage = async (req, res) => {
 
     res.render('organizations', { title, organizations });
 };
-
 
 const showOrganizationDetailsPage = async (req, res) => {
     const organizationId = req.params.id;
@@ -19,4 +19,23 @@ const showOrganizationDetailsPage = async (req, res) => {
 };
 
 
-export { showOrganizationsPage, showOrganizationDetailsPage };
+const showNewOrganizationForm = async (req, res) => {
+    const title = 'Add New Organization';
+
+    res.render('new-organization', { title });
+}
+
+const processNewOrganizationForm = async (req, res) => {
+    const { name, description, contactEmail } = req.body;
+    const logoFilename = 'placeholder-logo.png'; // Use the placeholder logo for all new organizations    
+
+    const organizationId = await createOrganization(name, description, contactEmail, logoFilename);
+    
+    // Set a success flash message
+    req.flash('success', 'Organization added successfully!');
+    
+    res.redirect(`/organization/${organizationId}`);
+};
+
+
+export { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm };
